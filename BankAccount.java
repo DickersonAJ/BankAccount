b1148f8bbff48b340e9cf2eced7b118d0dff4593
DickersonAJ <adickerson10@stuy.edu>
2020-10-27 20:15:13 -0400

    BankAccount.java compiled with all methods for real this time (forgot to add the file again)

--- BankAccount.java
+++ BankAccount.java
@@ -4,7 +4,7 @@ public class BankAccount {
   private int accountID;
   private String password;
   // Constructor
-  public Bank(int id, String pass) {
+  public BankAccount(int id, String pass) {
     balance = 0;
     accountID = id;
     password = pass;
@@ -21,5 +21,27 @@ public class BankAccount {
   public void setpassword(String pass) {
     password = pass;
   }
-  
+
+  public boolean deposit(double amount) {
+    if (amount >= 0) {
+      balance+=amount;
+      return true;
+    }
+    return false;
+  }
+
+  public boolean withdraw(double amount) {
+    if (amount >= 0) {
+      if (amount <= balance) {
+        balance-=amount;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public String toString() {
+    return (Integer.toString(getaccountID()) + "\t" + String.valueOf(getbalance()));
+  }
+
 }
